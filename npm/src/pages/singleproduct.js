02c95ce86dd65c2ba9a551b3f272e05e985f36f2
Ungerer221 <221302@virtualwindow.co.za>
2023-10-09 11:35:55 +0200

    Update singleproduct.js

--- npm/src/pages/singleproduct.js
+++ npm/src/pages/singleproduct.js
@@ -1,5 +1,14 @@
 import react from 'react'
 
+import Grid from '@mui/material/Grid';
+
 const SingleProduct = () => {
+    return (
+        <div className='singleproduct-page-main-con'>
+            <Grid container spacing={0}>
+
+            </Grid>
+        </div>
+    )
 
 }
