be0cc1b782fe83546485632458d4a9d4aca8b0b3
Rynoo1 <221361@virtualwindow.co.za>
2023-10-09 11:00:31 +0200

    update

--- server/index.js
+++ server/index.js
@@ -23,12 +23,12 @@ app.use('/api/parts', partsRoute);
 mongoose.connect(process.env.DB_CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: '', //Collection Name
+    dbName: 'DVTerm4Assessment', //Collection Name
 }).then(() => console.log("Connected to GlenSpares"))
     .catch((err) => {
         console.log("No Connection. Reason: " + err);
     });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 
 app.listen(PORT, () => {console.log(`Server has started at port: ${PORT}`)});
