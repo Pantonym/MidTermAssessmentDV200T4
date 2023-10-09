a4a5e40c257e8d04b462fbbbca96f420a48dc048
Rynoo1 <221361@virtualwindow.co.za>
2023-10-09 12:02:34 +0200

    singleproductpage

--- npm/src/pages/singleproduct.js
+++ npm/src/pages/singleproduct.js
@@ -5,24 +5,48 @@ import Button from '@mui/material/Button';
 
 
 const SingleProduct = () => {
+    const id = sessionStorage.getItem('ProductID');
+    console.log("Productid " + id);
+    const [product, setProduct] = useState([]);
+    const [cart, setCart] = useState([]);
+    const [quan, setQuan] = useState(1);
+  
+    // load data from db using product id in session storage
+    useEffect(() => {
+      Axios.get('http://localhost:5002/api/watch/' + id)
+        .then(result => {
+          setProduct(result.data);
+        })
+        .catch(err => console.log(err));
+  
+    }, []);
+
+    const handleAddCart = (key, brand, model, price, image, quantity) => {
+        const newItem = { key: key, brand: brand, model: model, image: image,price: price , quantity: quantity}
+        const existingCart = JSON.parse(sessionStorage.getItem('Cart')) || [];
+        const updatedCart = [...existingCart, newItem];
+        setCart(updatedCart);
+        sessionStorage.setItem('Cart', JSON.stringify(cart));
+        console.log(sessionStorage.getItem('Cart'));
+      };
     return (
         <div className='singleproduct-page-main-con'>
             <Grid container spacing={0}>
                 <Grid xs={6}></Grid>
                 <Grid xs={6}>
                     <Grid xs={12}>
-                        <h1>SPARE NAME</h1>
+                        <h1>{product.name}</h1>
                     </Grid>
                     <Grid xs={12}>
-                        <p>Car Name:</p>
-                        <p>Car Make:</p>
-                        <p>Chasis Number:</p>
-                        <p>Year</p>
+                        <p>{product.make}:</p>
+                        <p>{product.model}:</p>
+                        <p>{product.chasis}:</p>
+                        <p>{product.year}</p>
                     </Grid>
                     <Grid xs={12}>
-                        <h3>Price</h3>
+                        <h3>{product.price}</h3>
                     </Grid>
-                    <Button>Add to Cart</Button>
+                    <Button onClick={() => handleAddCart(product._id, product.name, product.make, product.model, product.price, product.image)} >Add to Cart</Button>
                 </Grid>
             </Grid>
         </div>
