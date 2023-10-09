db7082da280c2b0bbb6adb3757e0c6097ac0bf91
Pantonym <221179@virtualwindow.co.za>
2023-10-09 12:36:56 +0200

    products

--- npm/src/Components/ProductCard.js
+++ npm/src/Components/ProductCard.js
@@ -1,13 +1,27 @@
 import React from "react";
-import './Navbar.css';
+
+import { useState } from "react";
 
 import { Card } from 'react-bootstrap'
 
 const ProductCard = (props) => {
 
+    // Confirm click
+    const [oneClick, setOneClick] = useState(false);
+
+    const handleClick = () => {
+        if (!oneClick) {
+            setOneClick(true);
+            localStorage.setItem("ProductID", props.key);
+        } else {
+            console.log(localStorage.getItem("ProductID"));
+            window.location = '/SingleProduct';
+        }
+    };
+
     return (
         <Card>
-            <Card.Img variant="top" src="" />
+            <Card.Img variant="top" src={props.image} />
             <Card.Body>
                 <Card.Text>
                     <h3>{props.name}</h3>
@@ -16,7 +30,7 @@ const ProductCard = (props) => {
                     <h3>{props.chasis}</h3>
                     <h3>{props.year}</h3>
                     <h3>R{props.price}</h3>
-                    <button id={"btnCart" + props.key}>Add To Cart</button>
+                    <button id={"btnCart" + props.key} onClick={handleClick}> Add To Cart </button>
                 </Card.Text>
             </Card.Body>
         </Card>
--- npm/src/pages/products.js
+++ npm/src/pages/products.js
@@ -3,6 +3,8 @@ import './products.css';
 
 import { useState, useEffect } from "react";
 
+import { Button } from 'react-bootstrap'
+
 import Grid from '@mui/material/Grid';
 
 // Import Axios
@@ -15,6 +17,9 @@ const Products = () => {
     const [cards, setCards] = useState();
     const [reRenderProducts, setReRenderProducts] = useState(false);
 
+    const isAdmin = localStorage.getItem("User") === "Admin";
+    const [admin, setAdmin] = useState(isAdmin);
+
     useEffect(() => {
         Axios.get('http://localhost:5000/api/getparts')
             .then(res => {
@@ -22,7 +27,7 @@ const Products = () => {
                 console.log(productData);
 
                 let renderProducts = productData.map((item) =>
-                    <ProductCard key={item._id} id={item.partID} name={item.name} make={item.make} model={item.model} chasis={item.chasis} year={item.year} price={item.price}
+                    <ProductCard key={item._id} id={item.partID} name={item.name} make={item.make} model={item.model} chasis={item.chasis} year={item.year} price={item.price} image={item.image}
                     />)
 
                 setCards(renderProducts);
@@ -41,6 +46,71 @@ const Products = () => {
                     <Grid xs={12}>
                         {cards}
                     </Grid>
+
+                    {admin &&
+                        <Grid xs={12}>
+                            <Button variant="info" onClick={() => { document.getElementById('AddForm').style.display = "inline" }}>Add Product</Button>
+                        </Grid>
+                    }
+
+                    <div id="AddForm" style={{ display: "inline" }}>
+                        <form>
+                            <h2>Add Item</h2>
+
+                            <label> Name:
+                                <input type="text" name="name"></input>
+                            </label>
+
+                            <br></br>
+
+                            <label> Make:
+                                <input type="text" name="name"></input>
+                            </label>
+
+                            <br></br>
+
+                            <label> Model:
+                                <input type="text" name="name"></input>
+                            </label>
+
+                            <br></br>
+
+                            <label> Chasis:
+                                <input type="text" name="name"></input>
+                            </label>
+
+                            <br></br>
+
+                            <label> Year:
+                                <input type="text" name="name"></input>
+                            </label>
+
+                            <br></br>
+
+                            <label> Part ID:
+                                <input type="text" name="name"></input>
+                            </label>
+
+                            <br></br>
+
+                            <label> Stock:
+                                <input type="text" name="name"></input>
+                            </label>
+
+                            <br></br>
+
+                            <label> Price:
+                                <input type="text" name="name"></input>
+                            </label>
+
+                            <br></br>
+
+                            <label> Image:
+                                <input type="text" name="name"></input>
+                            </label>
+                        </form>
+                    </div>
+
                 </Grid>
 
             </div>
